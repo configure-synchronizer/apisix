d055c4858c519ecfd7336d319fb9d8d06c6326c2
storezhang <storezhang@gmail.com>
2025-10-28 12:45:43 +0800

    feat(项目): 初始化

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM mikefarah/yq:4.48.1 AS yq
+
+
+FROM ccr.ccs.tencentyun.com/dockerat/alpine:3.20.1 AS builder
+# 复制文件
+COPY --from=yq /usr/bin/yq /docker/usr/bin/yq
+COPY docker /docker
+
+
+
+# 打包真正的镜像
+FROM ccr.ccs.tencentyun.com/dockerat/alpine:3.20.1
+
+
+LABEL author="storezhang<华寅>" \
+    email="storezhang@gmail.com" \
+    qq="160290688" \
+    wechat="storezhang" \
+    description="网关配置刷新插件"
+
+RUN set -ex \
+    \
+    \
+    \
+    && apk update \
+    \
+    # 安装Git工具 \
+    && apk --no-cache add git openssh-client \
+    \
+    \
+    \
+    && rm -rf /var/cache/apk/*
+
+# 一次性复制所有文件，充分利用构建缓存，同时减少镜像层级
+COPY --from=builder /docker /
+
+# 所需环境变量
+ENV USERNAME ""
+ENV PASSWORD ""
+ENV REPLACE true
+ENV WORKDIR /tmp/apisix
+ENV TARGET /etc/apisix/conf.d
+ENV FILENAME apisix.yaml
+
+# 启动命令
+ENTRYPOINT ["/usr/local/bin/synchronizer.sh"]
---
+++ docker/usr/local/bin/synchronizer.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+workdir=${WORKDIR:-/tmp/apisix}
+target=${TARGET:-/etc/apisix/conf.d}
+filename=${FILENAME:-/apisix.yaml}
+interval="${INTERVAL:-5s}"
+replace="${REPLACE:-false}"
+merged_file=/tmp/merged.yaml
+pull=false
+
+# 嵌入脚本
+set -euo pipefail
+# shellcheck source=https://gitea.com/storezhang/script/raw/branch/main/core/log.sh
+source <(wget -q -O - https://gitea.com/storezhang/script/raw/branch/main/core/log.sh)
+
+log debug 启动配置加载器 "target: ${target}, interval: ${interval}"
+
+if [[ "${REMOTE}" =~ ^https?:// ]]; then
+  if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
+    tee "${HOME}/.netrc" <<-EOF > /dev/null
+default login ${USERNAME} password ${PASSWORD}
+EOF
+  else
+    log error 写入配置出错 "username: ${USERNAME}, password: ${PASSWORD}"
+  fi
+fi
+
+log info 创建工作目录 "dir: ${workdir}"
+mkdir -p "${workdir}"
+
+while true; do
+  if git -C "${workdir}" rev-parse --git-dir > /dev/null 2>&1; then
+    git -C "${workdir}" fetch
+
+    if git -C "${workdir}" status | grep -q "Your branch is behind"; then # 检查是否需要 pull
+      log info 更新代码 "dir: ${workdir}"
+      git -C "${workdir}" pull > /dev/null 2>&1 # 不显示输出
+      pull=true
+    fi
+  else
+    log info 检出代码 "dir: ${workdir}, remote: ${REMOTE}"
+    git clone "${REMOTE}" "${workdir}" > /dev/null 2>&1 # 不显示输出
+    pull=true
+  fi
+
+  if [ "${pull}" = true ]; then
+    mkdir -p "$(dirname ${merged_file})" > /dev/null 2>&1
+    touch ${merged_file}
+
+    log info 合并配置文件 "dir: ${workdir}"
+    find "${workdir}" -type f \( -name "*.yaml" -o -name "*.yml" \) -print0 | xargs -0  yq eval-all ". as \$item ireduce ({}; . *+ \$item)" > ${merged_file}
+
+    if yq --exit-status 'tag == "!!map" or tag == "!!seq"' "${merged_file}" >/dev/null; then
+      config_file=${target}/${filename}
+      if [ "${replace}" = true ]; then
+        log info 配置文件格式正确，替换新的配置 "to: ${config_file}, from: ${merged_file}"
+        cat "${merged_file}" > "${config_file}" # 写入文件内容
+      fi
+    else
+      log error 配置文件格式不正确 "new_file: $(cat ${merged_file})"
+    fi
+
+    pull=false
+  fi
+
+  # 等待指定间隔
+  log debug 等待重新加载配置文件 "interval: ${interval}"
+  sleep "${interval}"
+done
